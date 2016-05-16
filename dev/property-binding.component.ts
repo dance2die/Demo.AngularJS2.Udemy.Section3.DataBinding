35378043c36a59934e3268262628798400be235f
dance2die <dontbothermewithspam@gmail.com>
2016-05-16 07:14:08 -0400

    aliasing

--- dev/property-binding.component.ts
+++ dev/property-binding.component.ts
@@ -4,11 +4,11 @@ import {Component} from 'angular2/core';
     selector: 'my-property-binding',
     template: `
         <h2>This is the child component</h2>
-        <p>Hey {{myName}}! and {{myAge}} years old</p>
+        <p>Hey {{name}}! and {{age}} years old</p>
     `,
-    inputs: ['myName', 'myAge']
+    inputs: ['name:myName', 'age:myAge']
 })
 export class PropertyBindingComponent{
-    myName = '';
-    myAge = '';
+    name = '';
+    age = '';
 }
